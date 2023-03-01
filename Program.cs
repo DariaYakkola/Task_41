a92afe6b13c5cfbb689c61021f951920c14c9865
Daria Yakkola <yakkolabest@gmail.com>
2023-03-01 13:13:15 +0300

    Задача 41

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int count = 0;
+for (int i = 0; i < size; i++)
+{
+  Console.Write("Введите элемент массива: ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+  if (array[i] > 0)
+  {
+    count+=1;
+  }
+}
+Console.WriteLine($" В массиве [ {String.Join(" ", (array))} ] количество элементов больше 0 будет {count}");
